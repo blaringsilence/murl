578fd493f4c4ca2d93304006949fa61ff17b8aff
Mariam Maarouf <mariam@door2door.io>
2019-02-01 15:59:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name = 'mrf-murl',
   packages = ['mrf_murl'], 
-  version = '0.1',
+  version = '0.1.1',
   description = 'A URI manipulation tool aimed at web use.',
   author = 'Mariam Maarouf',
   author_email = 'mrf.mariam@gmail.com',
-  url = 'https://github.com/blaringsilence/murl',
-  download_url = 'https://github.com/blaringsilence/murl/tarball/0.1', 
+  url = 'https://github.com/mariamrf/murl',
+  download_url = 'https://github.com/mariamrf/murl/tarball/0.1.1', 
   keywords = ['uri', 'web'], 
   include_package_data = True,
   classifiers = [
